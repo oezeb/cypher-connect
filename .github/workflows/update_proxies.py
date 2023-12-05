2a16db7a09a3a86538bd1fb9a002e23780294bb5
oezeb <eouedraogo4@gmail.com>
2023-12-05 12:04:26 +0800

    black formatter

--- .github/workflows/update_proxies.py
+++ .github/workflows/update_proxies.py
@@ -10,6 +10,7 @@ from test_proxies import test_proxies
 CONFIG_URL = "https://raw.githubusercontent.com/oezeb/ClashX-V2Ray-TopFreeProxy/main/combine/clash.config.yaml"
 IP_API = "http://ip-api.com/json"
 
+
 def get(url):
     print("GET", url)
     try:
@@ -20,32 +21,33 @@ def get(url):
         print(e)
     return None
 
+
 def config2url(config):
     """Convert Shadowsocks config to URL"""
-    userInfo = '%s:%s' % (config['method'], config['password'])
-    host = '%s:%s' % (config['server'], config['server_port'])
+    userInfo = "%s:%s" % (config["method"], config["password"])
+    host = "%s:%s" % (config["server"], config["server_port"])
     plugin = None
-    if 'plugin' in config:
-        plugin = '%s;%s' % (config['plugin'], config['plugin_opts'])
+    if "plugin" in config:
+        plugin = "%s;%s" % (config["plugin"], config["plugin_opts"])
         plugin = urllib.parse.quote(plugin)
 
     userInfo = base64.b64encode(userInfo.encode()).decode()
-    userInfo = userInfo.replace('=', '')
+    userInfo = userInfo.replace("=", "")
 
-
-    url = 'ss://%s@%s' % (userInfo, host) 
+    url = "ss://%s@%s" % (userInfo, host)
     if plugin:
-        url += '?plugin=%s' % plugin
-    if 'name' in config:
-        url += '#%s' % config['name']
-    
+        url += "?plugin=%s" % plugin
+    if "name" in config:
+        url += "#%s" % config["name"]
+
     return url
+
 
 def main():
     content = get(CONFIG_URL)
     if content is None:
         return
-    
+
     data = yaml.load(content, Loader=yaml.FullLoader)
     proxies = [proxy for proxy in data["proxies"] if proxy["type"] == "ss"]
     results = test_proxies(proxies)
@@ -53,40 +55,43 @@ def main():
 
     text, names = "", defaultdict(int)
     for proxy in proxies:
-        print(proxy['name'])
-        if 'plugin' in proxy and proxy['plugin'] != 'obfs':
+        print(proxy["name"])
+        if "plugin" in proxy and proxy["plugin"] != "obfs":
             continue
 
         url = f"{IP_API}/{proxy['server']}?fields=status,countryCode,regionName"
         content = get(url)
         if content is None:
             continue
-        
+
         data = json.loads(content)
         if data["status"] == "fail":
             continue
 
-        name = data['countryCode']
-        if 'regionName' in data:
-            name += "-" + data['regionName']
+        name = data["countryCode"]
+        if "regionName" in data:
+            name += "-" + data["regionName"]
         names[name] += 1
         if names[name] > 1:
             sep = "-" if len(name) == 2 else " "
             name += sep + str(names[name])
 
-        proxy['name'] = urllib.parse.quote(name)
-        proxy['method'] = proxy.pop('cipher')
-        proxy['server_port'] = proxy.pop('port')
-        if 'plugin' in proxy:
-            proxy['plugin'] = 'obfs-local'
-            proxy['plugin-opts']['obfs'] = proxy['plugin-opts'].pop('mode')
-            proxy['plugin-opts']['obfs-host'] = proxy['plugin-opts'].pop('host')
-            proxy['plugin_opts'] = ';'.join(['%s=%s' % (k, v) for k, v in proxy['plugin-opts'].items()])
-        
+        proxy["name"] = urllib.parse.quote(name)
+        proxy["method"] = proxy.pop("cipher")
+        proxy["server_port"] = proxy.pop("port")
+        if "plugin" in proxy:
+            proxy["plugin"] = "obfs-local"
+            proxy["plugin-opts"]["obfs"] = proxy["plugin-opts"].pop("mode")
+            proxy["plugin-opts"]["obfs-host"] = proxy["plugin-opts"].pop("host")
+            proxy["plugin_opts"] = ";".join(
+                ["%s=%s" % (k, v) for k, v in proxy["plugin-opts"].items()]
+            )
+
         text += config2url(proxy) + "\n"
-    
+
     with open("proxies.txt", "w") as f:
         f.write(text)
-        
+
+
 if __name__ == "__main__":
     main()
